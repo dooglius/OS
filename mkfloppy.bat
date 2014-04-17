a75ecbef9cb7694b4de0f5e615981a1d58ccf7f9
dooglius <dooglius+github@gmail.com>
2014-04-17 10:51:54 -0400

    Get rid of file not needed

--- mkfloppy.bat
+++
@@ -1,6 +0,0 @@
-if exist blank.img del blank.img
-for /l %%i in (0,1,31) do echo 01234567890123456789012345678 >>blank1.img
-for /l %%i in (0,1,35) do type blank1.img >>blank2.img
-for /l %%i in (0,1,39) do type blank2.img >>blank.img
-del blank1.img
-del blank2.img
